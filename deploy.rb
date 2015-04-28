4c50d094e51e437125de26672103f7ceab5e59e4
showwin <showwin_kmc@yahoo.co.jp>
2015-04-28 14:41:11 +0900

    refactor & add working-check

--- deploy.rb
+++ deploy.rb
@@ -1,23 +1,31 @@
 require 'sinatra'
 require 'json'
 
+get '/' do
+  "I'm working !!!"
+end
+
 post '/' do
-  log_file = File.open("log.txt", "w")
-  json = JSON.parse(request.body.read) 
-  return if json["ref"] != "refs/heads/master"
+  log_file = File.open('log.txt', 'w')
+  json = JSON.parse(request.body.read)
+  return if json['ref'] != 'refs/heads/master'
   begin
     deploy
-    json["commits"].each do |commit|
-      log_file.write("commit '"+commit["message"]+"' was deployed. @"+Time.now.to_s)
+    json['commits'].each do |commit|
+      log_file.write("commit '" +
+                     commit['message'] +
+                     "' was deployed. @" +
+                     Time.now.to_s)
     end
   rescue
-    log_file.write("error! Master branch was not deployed successfully.")
+    log_file.write('error! Master branch was not deployed successfully.')
   end
   log_file.close
 end
 
 def deploy
-  path, pass = File.open("pathpass.txt", "r").read.split(" ").map{|str| str.chomp}
+  path, pass = File.open('pathpass.txt', 'r')
+                   .read.split(' ').map { |str| str.chomp }
   system("cd #{path} && git pull origin master")
   system("cd #{path} && rake assets:precompile RAILS_ENV=production")
   system("cd #{path} && rake db:migrate RAILS_ENV=production")
